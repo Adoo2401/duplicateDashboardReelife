d4945a4991c6a73d0d2136475d353fa3efcb6169
Adoo2401 <abdullahrehmani230@gmail.com>
2023-09-04 20:10:44 +0500

    try3

--- src/components/dashboard/analytics/AnalyticsTable.tsx
+++ src/components/dashboard/analytics/AnalyticsTable.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { Table, Badge, Button } from 'antd';
 import Link from 'next/link';
 import type { ColumnsType, TableProps } from 'antd/es/table';
-import { InfoCircleOutlined } from '@ant-design/icons';
 
+const { Column } = Table;
 
 interface TableRow {
     key: string;
@@ -12,23 +12,16 @@ interface TableRow {
     labels: React.ReactNode;
     reach: number;
     impression: number;
-    click:number
-    skip:number,
-    open:number
-    share:number,
-    complete:number,
-    activeUsers:number
 }
 
 
 const columns: ColumnsType<TableRow> = [
     {
       title: 'Story Group',
-      className:"text-[12px]",
       dataIndex: 'storyGroup',
       key: 'storyGroup',
       render: (text, { storyGroup }) => (
-        <Link href="interaction" className="flex items-center space-x-3 w-fit cursor-pointer">
+        <Link href="#" className="flex items-center space-x-3 w-fit cursor-pointer">
         <div className='flex items-center'>
             <div className="relative rounded-full overflow-hidden w-16 h-16">
                 <div className="absolute inset-0 border-2 border-[#bdbdca] rounded-full"></div>
@@ -48,8 +41,6 @@ const columns: ColumnsType<TableRow> = [
     },
     {
       title: 'Status',
-      className:"text-[12px]",
-      width:"100px",
       dataIndex: 'status',
       key: 'status',
       render: (text, { status }) => {
@@ -58,7 +49,6 @@ const columns: ColumnsType<TableRow> = [
     },
     {
       title: 'Labels',
-      className:"text-[12px]",
       dataIndex: 'labels',
       key: 'labels',
       render:(text, { labels }) => {
@@ -66,59 +56,16 @@ const columns: ColumnsType<TableRow> = [
       }
     },
     {
-      title:()=>{return <div className="flex space-x-2"><p>Reach</p><InfoCircleOutlined /> </div>},
-      className:"text-[12px]",
+      title: 'Reach',
       dataIndex: 'reach',
       key: 'reach',
       sorter: (a: TableRow, b: TableRow) => a.reach - b.reach,
     },
     {
-      title: ()=>{return <div className="flex space-x-2"><p>Impression</p><InfoCircleOutlined /> </div>},
-      className:"text-[12px]",
+      title: 'Impression',
       dataIndex: 'impression',
       key: 'impression',
       sorter: (a: TableRow, b: TableRow) => a.impression - b.impression,
-    },
-    {
-      title: ()=>{return <div className="flex space-x-2"><p>Click</p><InfoCircleOutlined /> </div>},
-      className:"text-[12px]",
-      dataIndex: 'click',
-      key: 'click',
-      sorter: (a: TableRow, b: TableRow) => a.impression - b.impression,
-    },
-    {
-      title: ()=>{return <div className="flex space-x-2"><p>Skip</p><InfoCircleOutlined /> </div>},
-      className:"text-[12px]",
-      dataIndex: 'skip',
-      key: 'skip',
-      sorter: (a: TableRow, b: TableRow) => a.impression - b.impression,
-    },
-    {
-      title: ()=>{return <div className="flex space-x-2"><p>Open</p><InfoCircleOutlined /> </div>},
-      className:"text-[12px]",
-      dataIndex: 'open',
-      key: 'open',
-      sorter: (a: TableRow, b: TableRow) => a.impression - b.impression,
-    },
-    {
-      title: ()=>{return <div className="flex space-x-2"><p>Share</p><InfoCircleOutlined /> </div>},
-      className:"text-[12px]",
-      dataIndex: 'share',
-      key: 'share',
-      sorter: (a: TableRow, b: TableRow) => a.impression - b.impression,
-    },
-    {
-      title: ()=>{return <div className="flex space-x-2"><p>Complete</p><InfoCircleOutlined /> </div>},
-      className:"text-[12px]",
-      dataIndex: 'complete',
-      key: 'complete',
-      sorter: (a: TableRow, b: TableRow) => a.impression - b.impression,
-    },
-    {
-      title: ()=>{return <div className="flex space-x-2"><p>Active Users</p><InfoCircleOutlined /> </div>},
-      className:"text-[12px]",
-      dataIndex: 'activeUsers',
-      key: 'activeUsers',
     },
   ];
 
@@ -130,27 +77,7 @@ const data: TableRow[] = [
         status:"Active",
         labels:"hi",
         reach:100,
-        impression:100,
-        click:19,
-        skip:289,
-        open:38,
-        share:2,
-        complete:3,
-        activeUsers:20
-    },
-    {
-        key: '2',
-        storyGroup:"",
-        status:"Disabled",
-        labels:"hi",
-        reach:49,
-        impression:392,
-        click:29,
-        skip:202,
-        open:219,
-        share:2,
-        complete:29,
-        activeUsers:202
+        impression:100
     },
     // Add more data rows as needed
 ];
@@ -158,8 +85,10 @@ const data: TableRow[] = [
 
 const AnalyticsTable = () => {
     return (
-        <div className='mt-20 max-w-full overflow-x-auto'>
-            <Table scroll={{x:true}} columns={columns} dataSource={data} size='small'/>
+        <div className='mt-20 px-6 max-w-full overflow-x-auto'>
+            <Table scroll={{x:true}} columns={columns} dataSource={data}>
+                
+            </Table>
         </div>
     )
 }
